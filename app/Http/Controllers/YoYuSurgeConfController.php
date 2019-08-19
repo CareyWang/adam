cad57556805d0f03ca67d6be87045a9fb85c20b5
CareyWong <xidiancc@gmail.com>
2019-08-19 13:57:20 +0800

    Fix: response

--- app/Http/Controllers/YoYuSurgeConfController.php
+++ app/Http/Controllers/YoYuSurgeConfController.php
@@ -13,9 +13,11 @@ class YoYuSurgeConfController extends Controller
         $eUrl = urlencode($url);
 
         $conf = File::get (base_path() . '/resources/files/YoYu.conf', $lock = false);
+        $conf = mb_convert_encoding($conf, 'UTF-8');
+
         $conf = str_replace(':url', $url, $conf);
         $conf = str_replace(':eUrl', $eUrl, $conf);
-        dump($conf);
-        return $conf;
+
+        return response($conf, 200)->header('Content-Type', 'text/html; charset=UTF-8');
     }
 }
